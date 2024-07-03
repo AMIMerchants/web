aa6355dc1b9550d39ff0d93ad6a722804511818f
TheCodizt <dev@thecodizt.com>
2024-07-03 14:01:14 +0530

    ADD: Base structure

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "American Merchants Inc.",
+  description: "Effortless payments, unmatched security, and global reach.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
