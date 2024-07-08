a57bd010fe5645124e3fd4fcd6ca72658a81323e
TheCodizt <dev@thecodizt.com>
2024-07-08 23:29:24 +0530

    FIX: Font size

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 	return (
 		<div className="h-full flex justify-between flex-col w-[90%]">
 			<div className="flex justify-between items-center py-5 px-[100px] bg-white rounded-lg shadow-lg min-h-[80vh]">
-				<div className="font-bold text-[48px] inline-block bg-gradient-to-r from-blue-600 to-blue-900 bg-clip-text text-7xl text-transparent">
+				<div className="font-bold inline-block bg-gradient-to-r from-blue-600 to-blue-900 bg-clip-text text-4xl text-transparent">
 					Seamless, secure and swift payment solutions for your
 					business.
 				</div>
