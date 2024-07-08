a85c943d3ae8646dfbe13c4ef777048321105f7f
TheCodizt <dev@thecodizt.com>
2024-07-08 08:43:46 +0530

    UPDATE: File locations

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      American Merchants Inc.
-    </div>
-  );
+	return <div>American Merchants Inc.</div>;
 }
